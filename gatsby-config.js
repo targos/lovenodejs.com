82cd72ab965da4640c4c012ba6b8f08fc8df3868
Michaël Zasso <targos@protonmail.com>
2019-03-07 14:21:48 +0100

    add GA tracking ID

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-26665739-2`,
       },
     },
     `gatsby-plugin-feed`,
